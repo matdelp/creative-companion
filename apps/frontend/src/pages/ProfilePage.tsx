98286947bbea96c5661ec0f53c3b344077b25c92
matdelp <mathilde.delpeuch@gmail.com>
2025-07-11 17:46:37 +0200

    feat: hook in profile page

--- apps/frontend/src/pages/ProfilePage.tsx
+++ apps/frontend/src/pages/ProfilePage.tsx
@@ -1,41 +1,29 @@
-import type { UserProfile } from "@creative-companion/common";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ArtworkCard } from "../components/profile/ArtworkCard";
 import { ProfileCard } from "../components/profile/ProfileCard";
+import { useGetUserProfile } from "../hooks/useGetUserProfile";
 
 export const ProfilePage: React.FC = () => {
-  const [user, setUser] = useState<UserProfile | null>(null);
+  const { data, isLoading, error } = useGetUserProfile();
 
-  useEffect(() => {
-    fetch("/api/artist/profile", { credentials: "include" })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setUser(data);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  }, []);
-  if (user == null) {
-    return null;
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+  if (error) {
+    return <div>error</div>;
   }
 
   return (
     <>
       <ProfileCard
-        picture={user.picture}
-        firstName={user.first_name}
-        lastName={user.last_name}
-        username={user.username}
-        description={user.description}
-        projects={user.artwork.length}
+        picture={data!.picture}
+        firstName={data!.first_name}
+        lastName={data!.last_name}
+        username={data!.username}
+        description={data!.description}
+        projects={data!.artwork.length}
       />
-      <ArtworkCard artworks={user.artwork} />
+      <ArtworkCard artworks={data!.artwork} />
     </>
   );
 };

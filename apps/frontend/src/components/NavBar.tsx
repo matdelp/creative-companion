01c23c3fc893ef53ddb23f910e8855870aa8eaf1
matdelp <mathilde.delpeuch@gmail.com>
2025-07-13 14:30:04 +0200

    add paintbrush icon

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { ProfileDropdown } from "./ProfileDropDown";
 import { useAuthStore } from "../store/authentication";
+import { Paintbrush } from "lucide-react";
 
 export const NavBar: React.FC = () => {
   const { isLoggedIn } = useAuthStore();
   return (
     <nav className="w-full text-whiteText-accent flex items-center justify-between px-4 py-3">
-      <div className="flex flex-col items-start text-lg font-bold">
-        <span className="">Creative </span>
-        <span>Companion</span>
+      <div className="flex items-center text-lg font-bold">
+        <Paintbrush className="text-myblue-400 mr-2" />
+        <div className="flex flex-col">
+          <span>Creative </span>
+          <span>Companion</span>
+        </div>
       </div>
       <ul className="flex items-center space-x-4">
         {isLoggedIn ? (

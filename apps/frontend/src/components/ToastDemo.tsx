6cd91987c84a25a68e00e08edd80411692788a46
matdelp <mathilde.delpeuch@gmail.com>
2025-10-17 14:52:47 +0200

    style: toast

--- apps/frontend/src/components/ToastDemo.tsx
+++ apps/frontend/src/components/ToastDemo.tsx
@@ -4,10 +4,18 @@ import "react-toastify/dist/ReactToastify.css";
 
 export const ToastDemo: React.FC = () => {
   const showToastMessage = () => {
-    toast.info(
+    toast(
       "Try the app by logging in: user:emma.boudet@email.com, psw:demototest",
       {
         position: "top-right",
+        style: {
+          background: "#FDE2E4",
+          color: "#DB2777",
+          fontWeight: "bold",
+          fontSize: "14px",
+          borderRadius: "12px",
+          padding: "12px 16px",
+        },
       }
     );
   };

f9079eef36aaca4d2a7780f97bf36abdc16dc9fb
matdelp <mathilde.delpeuch@gmail.com>
2025-10-20 16:45:44 +0200

    feat: style toast better

--- apps/frontend/src/components/ToastDemo.tsx
+++ apps/frontend/src/components/ToastDemo.tsx
@@ -8,20 +8,18 @@ type ToastDemoProps = {
 
 export const ToastDemo: React.FC<ToastDemoProps> = ({ demoButtonStyle }) => {
   const showToastMessage = () => {
-    toast(
-      "Try the app by logging in: user:emma.boudet@email.com, psw:demototest",
-      {
-        position: "top-right",
-        style: {
-          background: "#FDE2E4",
-          color: "#DB2777",
-          fontWeight: "bold",
-          fontSize: "14px",
-          borderRadius: "12px",
-          padding: "12px 16px",
-        },
-      }
-    );
+    toast(`Try the app by logging in:\nuser: demo@email.com\npsw: demototest`, {
+      position: "top-right",
+      style: {
+        background: "#FDE2E4",
+        color: "#DB2777",
+        fontWeight: "bold",
+        fontSize: "14px",
+        borderRadius: "12px",
+        padding: "12px 16px",
+        whiteSpace: "pre-line",
+      },
+    });
   };
 
   return (
